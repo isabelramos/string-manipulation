8eba0a98788eeaf4e3ff4882c0c85f582a3b0a53
Isabel Ramos <iramos1226@gmail.com>
2017-02-25 01:16:03 -0600

    finished assignment

--- strings.js
+++ strings.js
@@ -1,32 +1,40 @@
-var testString = "";
 var textInput = document.getElementById("enter-text");
 var showResult = document.getElementById("result");
 var button = document.getElementById("submit");
 
+function reversal () {
+	var textInput = document.getElementById("enter-text").value;
+	var backwardString = textInput.split("").reverse().join("");
+	showResult.innerHTML += (backwardString + "<br><br>");
+}
+
+function alphabits () {
+	var textInput = document.getElementById("enter-text").value;
+	var alphabeticalOrder = textInput.split("").sort().join("");
+	showResult.innerHTML += (alphabeticalOrder  + "<br><br>");
+}
+
+function palindrome () {
+	var textInput = document.getElementById("enter-text").value;
+	var isItAPalindrome = textInput.split("").reverse().join("");
+	if (textInput === isItAPalindrome) {
+		showResult.innerHTML += ("Your text is a palindrome!" + "<br><br>");
+	} else {
+		showResult.innerHTML += ("Your text is not a palindrome. :/" + "<br><br>");
+	}
+}
 
 function lettersOnly (clickEvent) {
+	var textInput = document.getElementById("enter-text").value;
 	var letters = /^[a-zA-Z]+$/;
-
-	if (textInput.value.match(letters)) {
-		console.log("blah");
+	if (textInput.match(letters)) {
+		reversal();
+		alphabits();
+		palindrome();
 	} else {
 		alert("This text field accepts letters only!");
 	}
 }
-
-
-// function reversal() {
-
-// }
-
-// function alphabits() {
-
-// }
-
-// function palindrome() {
-
-// }
-
 
 function pressEnter (event) {
     if (event.keyCode === 13) {
@@ -36,8 +44,3 @@ function pressEnter (event) {
 
 button.addEventListener("click", lettersOnly);
 textInput.addEventListener("keypress", pressEnter);
-
-
-		// reversal(testString);
-		// alphabits(testString);
-		// palindrome(testString);
